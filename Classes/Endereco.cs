93fcbdda2b2f95e5fb0238927eb2ffc6876cbe36
EdimiltonD <edimilton.sobrinho@oi.net.br>
2022-12-15 16:21:55 -0300

    aula remota 4

---
+++ Classes/Endereco.cs
@@ -0,0 +1,13 @@
+namespace backand.Classes
+{
+    public class Endereco
+    {
+        public string? Logradouro { get; set; }
+
+        public int Numero { get; set; }
+
+        public string? Complemento { get; set; }
+
+        public bool endComercial { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
 ﻿using backand.Classes;
 
 Pessoafisica novaPF  = new Pessoafisica();
+Endereco novoEndPf = new Endereco();
 
-novaPF.nome = "Millena menininha s2";
+novaPF.nome = "Edimilton dias ";
 novaPF.cpf = "000.000.000-2";
 novaPF.rendimento = 6600.5f;
+novaPF.dataNasc = new DateTime (2000, 02, 20);
+
+novoEndPf.Logradouro = " rua do forno";
+novoEndPf.Numero = 126;
+novoEndPf.Complemento = "Lu gifts";
+novoEndPf.endComercial = true;
+
+novaPF.endereco = novoEndPf;
+
+Console.WriteLine($"{novaPF.nome}, {novaPF.endereco}, {novoEndPf.Logradouro}");
+
+
+
 //Console.WriteLine(novaPF.nome);
 //Console.WriteLine(novaPF.cpf);
 
 
 //pessoa fisica  ============================================
-float impostoPagar = novaPF.CalcularImposto (novaPF.rendimento);
-Console.WriteLine(impostoPagar);
+//float impostoPagar = novaPF.CalcularImposto (novaPF.rendimento);
+//Console.WriteLine(impostoPagar);
 //pessoa juridica ===========================================
-Pessoajuridica novaPJ = new Pessoajuridica();
-Console.WriteLine(novaPJ.CalcularImposto(6600.5f));
+//Pessoajuridica novaPJ = new Pessoajuridica();
+//Console.WriteLine(novaPJ.CalcularImposto(6600.5f));
+
+//DateTime temp = new DateTime (2008, 01, 01);
+//Console.WriteLine(novaPF.ValidarDataNasc(temp));
